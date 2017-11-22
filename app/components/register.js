3e3925a0b1b1fb001c4183275f25046883bfa048
Alena Borisenko <alena.borisenko@gmail.com>
2017-11-22 00:50:38 -0500

    fixed indendation; added labels

--- app/components/register.js
+++ app/components/register.js
@@ -2,60 +2,59 @@ import React from 'react';
 
 export class RegisterPage extends React.Component {
   
-  constructor(props) {
-    super(props);
-      this.state = {
-        first: "",
-        last: "",
-        email: "",
-        password: "",
-        confirm:  "",
-        tuftsID: "",
-      };
-  }
-  encode(field, string) {
-    var newString = "";
-    for(var i = 0; i < string.length; i++) {
-      if(string[i] == '>') {
-        newString += '&gt';
-      }
-      else if (string[i] == '<') {
-        newString += '&lt';
-      }
-      else
-        newString += string[i];
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            first: "",
+            last: "",
+            email: "",
+            password: "",
+            confirm:  "",
+            tuftsID: "",
+        };
     }
-    this.update(field, newString)
-}
+    
+    encode(field, string) {
+        var newString = "";
+        for(var i = 0; i < string.length; i++) {
+            if(string[i] == '>') {
+                newString += '&gt';
+            }
+            else if (string[i] == '<') {
+                newString += '&lt';
+            }
+            else
+                newString += string[i];
+        }
+        this.update(field, newString)
+    }
 
-  update(field, value) {
+    update(field, value) {
 
-    this.setState({[field]: value});
-  }
+        this.setState({[field]: value});
+    }
 
-  encodeInput()
-  {
-    this.encode("first", this.state.first);
-    this.encode("last", this.state.last);
-    this.encode("email", this.state.email);
-    this.encode("tuftsID", this.state.tuftsID);
-  }
+    encodeInput() {
+        this.encode("first", this.state.first);
+        this.encode("last", this.state.last);
+        this.encode("email", this.state.email);
+        this.encode("tuftsID", this.state.tuftsID);
+    }
 
   render() {
 
     const container = {
-      display: 'block',
-      borderRadius: '10px 50px 30px',
-      background: '#FFFFFF',
-      marginLeft: '580px',
-      marginTop: '40px',
-      height: '470px',
-      width: '400px',
-      border: '1px black solid',
-      fontFamily: "Helvetica",
-      textAlign: 'center',
-      position: 'fixed',
+        display: 'block',
+        borderRadius: '10px 50px 30px',
+        background: '#FFFFFF',
+        marginLeft: '580px',
+        marginTop: '40px',
+        height: '470px',
+        width: '400px',
+        border: '1px black solid',
+        fontFamily: "Helvetica",
+        textAlign: 'center',
+        position: 'fixed',
     };
 
     const button_style = {
@@ -64,26 +63,56 @@ export class RegisterPage extends React.Component {
 	    fontSize: 32,
     };
     return (
-      <div className="register" style={container}>
-      <h1 style={{fontSize: '64px'}}> REGISTER </h1>
-      <div>
-        First Name: <input type="text" placeholder="Enter First Name" name="first" onChange={(event)=>this.update("first", event.target.value)} required/> 
-	<br /> <br />
-        Last Name: <input type="text" placeholder="Enter Last Name" name="last" onChange={(event)=>this.update("last", event.target.value)}required/>
-        <br /> <br />
-        Email: <input type="text" placeholder="Enter Tufts/Business Email" name="email" onChange={(event)=>this.update("email", event.target.value)} required/>
-        <br /> <br />
-	Password: <input type="password" placeholder="Enter Password" name="password" onChange={(event)=>this.update("password", event.target.value)}required/>
-        <br /> <br />
-	Confirm Password: <input type="password" placeholder="Confirm Password" name="confirm" onChange={(event)=>this.update("confirm", event.target.value)} required/>
-        <br /> <br />
-	Tufts ID: <input type="text" placeholder="Enter Tufts ID" name="tuftsID" onChange={(event)=>this.update("tuftsID", event.target.value)} required/>
-	<br /><br />
-      </div>
-      <div>
-        <button style={button_style} onClick={()=>this.encodeInput()}>Register</button>
-      </div>
-      </div>
+        <div className="register" style={container}>
+            <h1 style={{fontSize: '64px'}}> REGISTER </h1>
+            <div>
+                <label>First Name: </label>
+                <input type="text" 
+                       placeholder="Enter First Name" 
+                       name="first" 
+                       onChange={(event)=>this.update("first", event.target.value)} 
+                       required/> 
+                <br /><br />
+                <label>Last Name: </label>
+                <input type="text" 
+                       placeholder="Enter Last Name" 
+                       name="last" 
+                       onChange={(event)=>this.update("last", event.target.value)} 
+                       required/>
+                <br /><br />
+                <label>Email: </label> 
+                <input type="text" 
+                       placeholder="Enter Tufts/Business Email" 
+                       name="email" 
+                       onChange={(event)=>this.update("email", event.target.value)} 
+                       required/>
+                <br /><br />
+                <label>Password: </label>
+                <input type="password" 
+                       placeholder="Enter Password" 
+                       name="password" 
+                       onChange={(event)=>this.update("password", event.target.value)}
+                       required/>
+                <br /><br />
+                <label>Confirm Password: </label>
+                <input type="password" 
+                       placeholder="Confirm Password" 
+                       name="confirm" 
+                       onChange={(event)=>this.update("confirm", event.target.value)} 
+                       required/>
+                <br /><br />
+                <label>Tufts ID: </label>
+                <input type="text" 
+                       placeholder="Enter Tufts ID" 
+                       name="tuftsID" 
+                       onChange={(event)=>this.update("tuftsID", event.target.value)} 
+                       required/>
+                <br /><br />
+            </div>
+            <div>
+                <button style={button_style} onClick={()=>this.encodeInput()}>Register</button>
+            </div>
+        </div>
     )
   }
 }
--- app/components/signin.js
+++ app/components/signin.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-
 export class SignIn extends React.Component {
 	constructor(props) {
 	    super(props);
@@ -10,43 +9,50 @@ export class SignIn extends React.Component {
 		    password: "",
 		};
 	}
-	 update(field, value) {
+	update(field, value) {
     	this.setState({[field]: value});
   	}
 
-
-
 	render() {
 	    const login_container = {
-		display: 'block',
-		borderRadius: '20px',
-		margin: '100px',
-		marginLeft: '200px',
-		marginTop: '150px',
-		height: '400px',
-		width: '400px',
-		background: "#3D5AFE",
-		fontFamily: "Helvetica",
-		color: 'white',
-		textAlign: 'center',
-		padding: '5px',
-		position: 'fixed',
+			display: 'block',
+			borderRadius: '20px',
+			margin: '100px',
+			marginLeft: '200px',
+			marginTop: '150px',
+			height: '400px',
+			width: '400px',
+			background: "#3D5AFE",
+			fontFamily: "Helvetica",
+			color: 'white',
+			textAlign: 'center',
+			padding: '5px',
+			position: 'fixed',
 	    };
 
 	    const button_style = {
-		background: '#1A237E',
-		color: 'white',
-		fontSize: 32,
+			background: '#1A237E',
+			color: 'white',
+			fontSize: 32,
 	    };
 
 	    return (
     		<div className="signin" style={login_container}>
-			<h1 style={{fontSize: '64px', textAlign: 'center'}}>LOGIN</h1>
-			<h2 style={{fontSize: '21px'}}>Welcome back</h2>
-    			<div>Email: <input name="email" type="text" onChange={(event)=>this.update("email", event.target.value)} /> <br /> <br />
-			    Password: <input name="password" type="text" onChange={(event)=>this.update("password", event.target.value)} /> <br /> <br />
-			    <button style={button_style}>Log In</button>
-			</div>
+				<h1 style={{fontSize: '64px', textAlign: 'center'}}>LOGIN</h1>
+				<h2 style={{fontSize: '21px'}}>Welcome back</h2>
+    			<div>
+    				<label>Email: </label>
+    				<input name="email" 
+    					   type="text" 
+    					   onChange={(event)=>this.update("email", event.target.value)} /> 
+	    			<br /><br />
+				    <label>Password: </label>
+				    <input name="password" 
+				    	   type="text" 
+				    	   onChange={(event)=>this.update("password", event.target.value)} /> 
+				    <br /><br />
+				    <button style={button_style}>Log In</button>
+				</div>
     		</div>
         );
  	}
